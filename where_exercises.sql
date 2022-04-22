d9eff4266cb2383399eb2ad6c821a74efe2aaa9a
Kaylah Andrews <kandr716@gmail.com>
2022-04-21 23:08:30 -0500

    complete exercises

---
+++ where_exercises.sql
@@ -0,0 +1,32 @@
+USE employees;
+
+SELECT * FROM employees
+WHERE first_name IN ('Irena','Vidya','Maya');
+
+SELECT * FROM employees
+    WHERE last_name LIKE 'e%';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees
+WHERE first_name = 'Irena'
+    OR  first_name = 'Vidya'
+    OR first_name = 'Maya';
+
+SELECT * FROM employees
+WHERE gender = 'M'
+  AND (first_name = 'Irena'
+    OR  first_name = 'Vidya'
+    OR first_name = 'Maya');
+
+SELECT * FROM employees
+    WHERE last_name LIKE 'e%'
+    OR last_name LIKE '%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name not like '%qu%';
