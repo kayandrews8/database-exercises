168fb30cf38018a8176fe13fba24bf70a1eb4bcb
Kaylah Andrews <kandr716@gmail.com>
2022-04-23 22:25:55 -0500

    complete limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+
+SELECT emp_no from salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+
+SELECT emp_no from salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
+
+
